cda0f82b5076d5cd2919995b1747d8c8c38d24f1
GabrielGalindo1 <gabrieldelimagalindo.gdlg@gmail.com>
2022-02-08 19:22:48 -0300

    Foram feitas constantes de todos os usuarios, incluindo nome,idade,peso,altura,se é dev ou não, logo em seguida somei as idades, agrupei os nomes e tabelei,fiz o calculo de imc separadamente tabelados, defini por true e false quem é dev e quem não é, e usei um if para imprimir na tabela quem tem o sobrenome silva

--- src/index.ts
+++ src/index.ts
@@ -6,6 +6,7 @@ import * as dotenv from "dotenv";
 import express from "express";
 import cors from "cors";
 import helmet from "helmet";
+import e from "cors";
 
 dotenv.config();
 
@@ -36,8 +37,44 @@ app.use(express.json());
 app.listen(PORT, () => {
 	console.log(`Listening on port ${PORT}`);
 
+	const nome1: string = 'UbuntuSilva';
+	const idade1: number = 33;
+	const peso1: number = 88.50;
+	const altura1: number = 1.65;
+	const ehDev1: boolean= true;
 
+	const nome2: string = 'UbuntuSilva';
+	const idade2: number = 30;
+	const peso2: number = 75.00;
+	const altura2: number = 1.75;
+	const ehDev2: boolean= true;
+
+	const nome3: string = 'UbuntuMachado';
+	const idade3: number = 22;
+	const peso3: number = 90.00;
+	const altura3: number = 1.75;
+	const ehDev3: boolean= false;
 	// CÓDIGO PARA ATENDER OS REQUERIMENTOS
 	// R01, R02, R03, R04, R05
+	console.log("=== Soma das idades ===")
+	console.log(idade1+idade2+idade3)
+
+	console.log("=== Agrupamento de nomes ===")
+	console.table([nome1,nome2,nome3])
+
+	console.log("=== IMC Ubuntu Silva1  ===")
+	console.log(peso1 / (altura1 * altura1))
+	console.log("=== IMC Ubuntu Silva2  ===")
+	console.log(peso2 / (altura2 * altura2))
+	console.log("=== IMC Ubuntu Machado  ===")
+	console.log(peso3 / (altura3 * altura3))
+
+	console.log("=== Quantidade de devs ===")
+	console.table([ehDev1, ehDev2, ehDev3])
+
+	if(nome1 && nome2 == 'UbuntuSilva'){
+		console.log("=== Temos 2 silvas na lista ===")
+		console.table([nome1, nome2])
+	}
 	
 });
